67f7764be20165303191c0accd8ac8120468bfe7
AlexanderSuslikov <suslikov.alexander@gmail.com>
2016-12-06 01:32:25 +0300

    I added 2 activity(shops menu)

--- Application/src/main/java/com/example/alexander/firstapp/MainActivity.java
+++ Application/src/main/java/com/example/alexander/firstapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.alexander.firstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button buttonNewRout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonNewRout = (Button) findViewById(R.id.buttonNewRout);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonNewRout:
+                Intent intent = new Intent(this, ShopsActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
---
+++ Application/src/main/java/com/example/alexander/firstapp/ShopsActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexander.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShopsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+    }
+}
